b941aea8c5b1459d672e2aff355bc131e91cd721
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-12-18 14:42:22 +0300

    update frontend urls

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -3,5 +3,17 @@ from .views import index
 
 urlpatterns = [
     # path('<path:route>', index),
+    path('', index),
+    path('signUp/', index),
+    path('registration/', index),
+    path('catalogWorker/', index),
     path('catalogAdmin/', index),
+    path('fileWorker/', index),
+    path('fileAdmin/', index),
+    path('profileWorker/', index),
+    path('profileAdmin/', index),
+    path('listWorker/', index),
+    path('newProfile/', index),
+    path('createProfile/', index),
+    path('editProfile/', index),
 ]
