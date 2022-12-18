c2628d2113bfe9be44d9baec2b91221a8551a0c1
profatsky <mixail.rostov04@gmail.com>
2022-12-18 04:24:41 +0300

    added settings for email sending

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,9 +137,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email sending
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+
 EMAIL_HOST_USER = ''
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = ''
 
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
